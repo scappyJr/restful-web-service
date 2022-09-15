f80eb97d15d7d72420107384b0fd3e27ef00fef8
scappyJr <terroir713@gmail.com>
2022-09-15 14:20:40 +0900

    # HelloWorldBean, HelloWorldController 추가 - RestController, Lombok 이용

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.example.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    /**
+     * Get /hello-world (endpoint)
+     * 이전 방식 -> @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+     */
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    /**
+     * RestController 이용 시 별도로 ResponseBody 값을 반환하지 않아도
+     * Class, Collection 등의 객체를 JSON 형태로 반환한다.
+     * (XML 형태로 반환하려는 경우 별도의 라이브러리 추가 필요)
+     */
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello world!");
+    }
+    
+}
