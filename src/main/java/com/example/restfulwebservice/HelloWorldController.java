9061c050c900340701b36b16101b961a749dafc2
scappyJr <terroir713@gmail.com>
2022-09-16 15:02:52 +0900

    # HelloWorld Controller에 PathVariable 이용한 API 추가

--- src/main/java/com/example/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.restfulwebservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,5 +25,10 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello world!");
     }
-    
+
+    // path variable 을 이용하여 가변 uri 처리
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello world %s!", name));
+    }
 }
