fda9063bf565ef56d870ed1ecbffe96499b717d6
scappyJr <terroir713@gmail.com>
2022-09-19 15:43:52 +0900

    # User 목록 조회를 위한 API 구현 - GET HTTP Method

---
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -0,0 +1,41 @@
+package com.example.restfulwebservice.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private UserDaoService service;
+
+    /**
+     * 생성자를 통한 의존성 주입
+     * @param service
+     */
+    public UserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    /**
+     * 전체 User 목록 조회
+     * @return
+     */
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    /**
+     * 단일 User 조회
+     * @param id 사용자 ID
+     * @return
+     */
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+
+}
