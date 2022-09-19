8830c70b985beb45b2cbcbe5a6512303bc0cff0d
scappyJr <terroir713@gmail.com>
2022-09-19 16:24:22 +0900

    # User 생성 시 HTTP Status Code 제어 처리 및 Response Header에 생성된 User의 ID 값 반환

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,7 +1,10 @@
 package com.example.restfulwebservice.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -40,8 +43,15 @@ public class UserController {
      * User 생성
      */
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
