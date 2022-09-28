a389bc63969aea5f888111ba2f66e331f4e9abe8
scappyJr <terroir713@gmail.com>
2022-09-28 16:56:50 +0900

    # JPA 사용을 위한 Dependency 추가와 설정

--- src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -2,14 +2,31 @@ package com.example.restfulwebservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    /**
+     * Spring Boot Security 적용 후 별도의 인증 없이 h2-console 접속 허용
+     * @param http
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        return http.build();
+    }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
