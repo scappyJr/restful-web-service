358e0741f11431c9abbbe0d7ad30284104c3fef5
scappyJr <terroir713@gmail.com>
2022-09-29 17:50:58 +0900

    # Spring Data JPA를 이용한 Entity 설정과 초기 데이터 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into member values(1, now(), 'member1', 'test111', '801010-1111111');
+insert into member values(2, now(), 'member2', 'test222', '901010-1111111');
+insert into member values(3, now(), 'member3', 'test333', '951010-2222222');
